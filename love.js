6ebae06d4d40ffb1ce0406081242fc4757dbf224
ridoyvhila <ridoyvhila51@gmail.com>
2020-02-16 00:06:54 +0600

    love commit

---
+++ love.js
@@ -0,0 +1,14 @@
+
+
+//Love culculator
+
+
+let fastname = prompt("What is your name? ");
+let Partnername = prompt("What is your Partner name?");
+
+let number = Math.random() *100;
+
+number = Math.floor(number) +1;
+
+alert(fastname + " and " + Partnername + "'s love percentage parson:" 
+	+ number + "%");
